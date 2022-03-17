308f238e04caf7c6f3fcbc8a71a7e77130f074ca
bruno lapierre <bruno.lapierre@gallea.ca>
2022-03-17 15:48:26 -0400

    Events + routes changes

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Events Listing</h1>
+    <ul>
+      <li><router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link></li>
+      <li> <router-link :to="{ name: 'event-show', params: { id: '2' } }">Show Event #2</router-link></li>
+      <li><router-link :to="{ name: 'event-show', params: { id: '3' } }">Show Event #3</router-link></li>
+    </ul>
+  </div>
 </template>
